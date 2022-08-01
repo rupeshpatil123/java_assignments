1d6be9a841680b311453f862790a72880343f9eb
rupeshpatil123 <109964885+rupeshpatil123@users.noreply.github.com>
2022-08-01 08:17:05 +0530

    Add files via upload
    Employee Management guided assignment 30july frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+ import {Router, RouterModule, Routes  } from "@angular/router";
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SalesPersonListComponent } from './sales-person-list/sales-person-list.component';
+import { EmpListComponent } from './components/emp-list/emp-list.component';
+import { DeptListComponent } from './components/dept-list/dept-list.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { EmpFormComponent } from './components/emp-form/emp-form.component';
+import { FormsModule } from '@angular/forms';
 
+const route : Routes = [
+  {path:'empform',component:EmpFormComponent},
+  {path: '',component:WelcomeComponent},
+  {path : 'employees',component:EmpListComponent},
+  {path : 'departments',component:DeptListComponent}
+]
 @NgModule({
   declarations: [
     AppComponent,
-    SalesPersonListComponent
+    EmpListComponent,
+    DeptListComponent,
+    WelcomeComponent,
+    EmpFormComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(route)
   ],
   providers: [],
   bootstrap: [AppComponent]
